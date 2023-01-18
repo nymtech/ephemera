48c27c6e1c93191d9562aaab41517ffaf819cb78
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2023-01-18 18:40:45 +0000

    Giving a bit of feedback at client start

--- examples/console-client/src/main.rs
+++ examples/console-client/src/main.rs
@@ -1,8 +1,10 @@
-use ephemera_client::{cli, RbClient};
 use ephemera_client::cli::Commands;
+use ephemera_client::{cli, RbClient};
 
 #[tokio::main]
 async fn main() {
+    println!("Starting client, enter data and hit enter to broadcast...");
+
     let args = cli::parse_args();
     match args.command {
         Commands::Broadcast { node_address } => {
