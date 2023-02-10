0c317b282bd758d2ffe442ab443f80785c42b4da
Dave Hrycyszyn <futurechimp@users.noreply.github.com>
2023-02-10 17:05:43 +0000

    rustfmt

--- node/src/block/callback.rs
+++ node/src/block/callback.rs
@@ -8,18 +8,27 @@ pub(crate) struct ApplicationInfo<'a> {
 
 impl<'a> ApplicationInfo<'a> {
     pub(crate) fn new(label: Option<String>, messages: &'a [SignedMessage]) -> Self {
-        Self { label, accepted_messages: messages }
+        Self {
+            label,
+            accepted_messages: messages,
+        }
     }
 }
 
-pub(crate) trait BlockProducerCallback{
-    fn on_proposed_block<'a>(&self, messages: &'a [SignedMessage]) -> anyhow::Result<Option<ApplicationInfo<'a>>>;
+pub(crate) trait BlockProducerCallback {
+    fn on_proposed_block<'a>(
+        &self,
+        messages: &'a [SignedMessage],
+    ) -> anyhow::Result<Option<ApplicationInfo<'a>>>;
 }
 
 pub(crate) struct DummyBlockProducerCallback;
 
 impl BlockProducerCallback for DummyBlockProducerCallback {
-    fn on_proposed_block<'a>(&self, messages: &'a [SignedMessage]) -> anyhow::Result<Option<ApplicationInfo<'a>>> {
+    fn on_proposed_block<'a>(
+        &self,
+        messages: &'a [SignedMessage],
+    ) -> anyhow::Result<Option<ApplicationInfo<'a>>> {
         let accepted = ApplicationInfo::new(None, messages);
         Ok(Some(accepted))
     }
@@ -28,10 +37,13 @@ impl BlockProducerCallback for DummyBlockProducerCallback {
 pub(crate) struct MinMessageCountBlockProducerCallback(pub(crate) usize);
 
 impl BlockProducerCallback for MinMessageCountBlockProducerCallback {
-    fn on_proposed_block<'a>(&self, messages: &'a [SignedMessage]) -> anyhow::Result<Option<ApplicationInfo<'a>>> {
+    fn on_proposed_block<'a>(
+        &self,
+        messages: &'a [SignedMessage],
+    ) -> anyhow::Result<Option<ApplicationInfo<'a>>> {
         if messages.len() >= self.0 {
             let accepted = ApplicationInfo::new(None, messages);
-            return Ok(Some(accepted))
+            return Ok(Some(accepted));
         }
         Ok(None)
     }
