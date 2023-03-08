db88e88c1395c720e6fb794a7a5f0787d46c3554
Andrus Salumets <andrus@nymtech.net>
2023-03-08 10:22:53 +0000

    Add shutdown to cli ephemera runner

--- node/src/cli/run_node.rs
+++ node/src/cli/run_node.rs
@@ -2,10 +2,10 @@ use std::path::PathBuf;
 use std::sync::Arc;
 
 use clap::Parser;
+use tokio::signal::unix::{signal, SignalKind};
 
 use crate::api::application::{Application, DefaultApplication};
 use crate::api::types::{ApiBlock, ApiEphemeraMessage, ApiEphemeraRawMessage};
-
 use crate::config::Configuration;
 use crate::core::builder::EphemeraBuilder;
 use crate::utilities::crypto::ed25519::Ed25519Keypair;
@@ -22,7 +22,7 @@ impl RunExternalNodeCmd {
     pub async fn execute(&self) {
         let conf = match Configuration::try_load(PathBuf::from(self.config_file.as_str())) {
             Ok(conf) => conf,
-            Err(err) => panic!("Error loading configuration file: {err:?}",),
+            Err(err) => panic!("Error loading configuration file: {err:?}", ),
         };
 
         let ephemera = EphemeraBuilder::new(conf)
@@ -31,7 +31,26 @@ impl RunExternalNodeCmd {
             .await
             .unwrap();
 
-        tokio::spawn(ephemera.run()).await.unwrap();
+        let mut ephemera_shutdown = ephemera.ephemera_handle.shutdown.clone();
+
+        let ephemera_handle = tokio::spawn(ephemera.run());
+
+        let shutdown = async {
+            let mut stream_int = signal(SignalKind::interrupt()).unwrap();
+            let mut stream_term = signal(SignalKind::terminate()).unwrap();
+            tokio::select! {
+            _ = stream_int.recv() => {
+                ephemera_shutdown.shutdown();
+            }
+            _ = stream_term.recv() => {
+               ephemera_shutdown.shutdown();
+            }
+        }
+        };
+
+        //Wait shutdown signal
+        shutdown.await;
+        ephemera_handle.await.unwrap();
     }
 }
 
